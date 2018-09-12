69eab899a4eb46bb97065592c93ccdfc91632c24
Diogo Jose Costa Alves <diogo.alves@ancinerj.gov.br>
2018-09-12 17:05:18 -0300

    add inital todo list

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// TODO selected unicode subset
+// TODO create a huffman tree list from my webhistory links or hackernews links
+// TODO create a table/array to encode tree symbols with unicode subset
+// TODO make / with a screen to encode
+// TODO /encode/:xxxx to encode xxx
+// TODO /:xxxx to redirect to decoded 
 class App extends Component {
   render() {
     return (
@@ -12,10 +18,22 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          
+          {String.fromCharCode(41)}
         </p>
       </div>
     );
   }
 }
+
+/*
+
+for(i=0;i<65536;i++) {
+    console.log(i.toString(16))
+}
+
+*/
+
+
 
 export default App;
