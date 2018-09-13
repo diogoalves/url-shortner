7ffef67c4298bfe8bccd935b9b4df80c57fcc28f
casa <diogo.alves@gmail.com>
2018-09-13 06:05:22 -0300

    feat: add a fallback function to generate static unicode chars

--- src/util.js
+++ src/util.js
@@ -1,17 +1,28 @@
 
-const test = char => !( /[\x00-\x08\x0E-\x1F\x80-\xFF]/.test(char)) && !/^\p{White_Space}/u.test(char);
+const test = char => !(/[\x00-\x08\x0E-\x1F\x80-\xFF]/.test(char)) && !/^\p{White_Space}/u.test(char);
 
-const generateUnicodeChars2 = () => {
-  const range =Array.from(Array(65536).keys()); //returns keys of an Array with 65536 roles, like [0,1,...,65536]
+const generateUnicodeChars3 = () => {
+  const range = Array.from(Array(65536).keys()); //returns keys of an Array with 65536 roles, like [0,1,...,65536]
 
-    return range.reduce( (acc, cur, index) => {
+  return range.reduce((acc, cur, index) => {
     const c = String.fromCharCode(index);
-    if(test(c)) {
+    if (test(c)) {
       return acc.concat(c);
     } else {
       return acc;
     }
-  }, [] );
-} 
+  }, []);
+}
+
+const generateUnicodeChars = () => {
+  let ret = [];
+  for (i = 0; i < 65536; i++) {
+    const c = String.fromCharCode(i);
+    if (test(c)) {
+      ret.push(c);
+    }
+  }
+  return ret;
+}
 
 
