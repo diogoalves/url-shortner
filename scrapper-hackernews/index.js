9f65c22587d86f5c03efa75c10ee4db44567a059
casa <diogo.alves@gmail.com>
2018-09-17 05:16:25 -0300

    fix: partition fetchs in windows to avoid memory 1800000 size array

--- scrapper-hackernews/index.js
+++ scrapper-hackernews/index.js
@@ -1,27 +1,44 @@
 const firebase = require("firebase");
-const pLimit = require('p-limit');
+const pMap = require('p-map');
 
 const HN_DATABASE_URL = 'https://hacker-news.firebaseio.com';
 const MAX_CONCURRENT_PROMISES = 2000;
 
+
 const app = firebase.initializeApp({ databaseURL: HN_DATABASE_URL }, 'hackernews');
 const db = app.database();
 const maxttemRef = db.ref("v0/maxitem");
-const getItem = itemId => db.ref(`v0/item/${itemId}`).limitToLast(1).once("value");
-const limit = pLimit(MAX_CONCURRENT_PROMISES);
 
-(async () => {
-  const lastId = (await maxttemRef.once("value")).val();
-
-  const items = Array.from(new Array(lastId), (x,i) => i+1);
-  const actions = items.map( i => limit(() => getItem(i)))
-
+const process_windows = async (actions) => {
+  console.log("processing " + actions.length + "actions")
   const result = await Promise.all(actions);
-  result.map( item => {
+  console.log("PROCESSOU")
+  result.map(item => {
     const { url } = item.val();
-    if(url) {
+    if (url) {
       console.log(url)
     }
   });
+}
+
+(async () => {
+  const lastId = (await maxttemRef.once("value")).val();
+  console.log(lastId)
+
+  const getItem = async itemId => {
+    const result = await db.ref(`v0/item/${itemId}`).limitToLast(1).once("value");
+    const { url } = result.val();
+    if (url) {
+      console.log(url)
+    }
+  }
+
+  const WINDOW_SIZE = 1000;
+  const WINDOWS_QTY = Math.ceil(lastId/WINDOW_SIZE);
+  for(var w=0; w<WINDOWS_QTY; w++) {
+    const items = Array.from(new Array(WINDOW_SIZE), (x, i) => i + 1 + (w*WINDOW_SIZE));
+    await pMap(items, getItem, { concurrency: 100 });
+  }
+
   process.exit();
-})();
+}) ();
